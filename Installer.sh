143043107469d86a551b97be92eb7f4751f34403
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2023-11-13 14:57:00 -0500

    Create Installer.sh

---
+++ Installer.sh
@@ -0,0 +1,7 @@
+ruby -v || echo "Error: Ruby is not installed or is not on your path! Please install Ruby and try again https://rubyinstaller.org/"
+
+gem install glimmer -v2.7.3
+
+echo "Ruby has been sucessfully installed, use require 'glimmer' to run Glimmer in your Ruby program!"
+
+echo "Configuration Glimmer configuration may be done via the Glimmer::Config module."
