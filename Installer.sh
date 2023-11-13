74260f583ff0b090b525ddbf041ffd507929cf5a
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2023-11-13 15:02:17 -0500

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -5,3 +5,25 @@ gem install glimmer -v2.7.3
 echo "Ruby has been sucessfully installed, use require 'glimmer' to run Glimmer in your Ruby program!"
 
 echo "Configuration Glimmer configuration may be done via the Glimmer::Config module."
+
+
+PS3='Select your OS: '
+options=("Ubuntu/Debian 1" "CentOS, Fedora, or RHEL 2" "Gentoo 3" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Ubuntu/Debian 1 1")
+            echo "you chose choice 1"
+            ;;
+        "CentOS, Fedora, or RHEL  2")
+            echo "you chose choice 2"
+            ;;
+        "Gentoo 3")
+            echo "you chose choice $REPLY which is $opt"
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
